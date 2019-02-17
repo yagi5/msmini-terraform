62a108ab21f0bd44e70cf5d887855f0136cacb95
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2019-02-17 22:53:34 +0900

    fix data

--- terraform/platform/manchester/google_project.tf
+++ terraform/platform/manchester/google_project.tf
@@ -1,6 +1,6 @@
 resource "google_project" "manchester" {
   name        = "manchester"
   project_id  = "manchester"
-  folder_id   = "${google_folder.msmini.name}"
+  folder_id   = "${data.google_folder.msmini.name}"
   skip_delete = false
 }
