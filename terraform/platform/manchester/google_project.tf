7b2eacc84ebf577b9ba90ec3ef971612a8819b3c
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2019-02-17 23:21:17 +0900

    fix folder

--- terraform/platform/manchester/google_project.tf
+++ terraform/platform/manchester/google_project.tf
@@ -2,6 +2,6 @@ resource "google_project" "manchester" {
   // manchester is gke cluster pj
   name        = "manchester"
   project_id  = "manchester"
-  folder_id   = "folder/1080284529756"
+  folder_id   = "folders/1080284529756"
   skip_delete = false
 }
